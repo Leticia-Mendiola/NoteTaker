9986a520ce6915beee73efd48d28654ff9c6a007
Leticia-Mendiola <lmendiola509@gmail.com>
2021-04-12 22:12:27 -0500

    added push functionality that should hopefully push new notes to the saved notes

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ app.use(express.json());
 
 app.get('/api/notes', (req, res) => {
     return res.json(notes);
-  });
+});
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    console.log(newNote);
+    notes.push(newNote);
+    res.json(newNote);
+});
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
