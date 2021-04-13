a253118853dccefbb82540dc1f18a244ee13c3d0
Leticia-Mendiola <lmendiola509@gmail.com>
2021-04-12 23:01:03 -0500

    removed unnecessary code

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const path = require('path');
 
-// const notes = require('./db/db.json');
-
 const app = express();
 const PORT = 8080;
 
@@ -11,8 +9,7 @@ app.use(express.json());
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
-
-app.get('/api/notes', (req, res) => res.json(notes));
+app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, '/db/db.json')));
 
 app.post('/api/notes', (req, res) => {
     const newNote = req.body;
