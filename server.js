49bcc796901b83c3f8f1b2088438b55483b393c9
Leticia-Mendiola <lmendiola509@gmail.com>
2021-04-12 22:06:28 -0500

    required db.json file and created get function to retrieve notes

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
-
+const notes = require('./db/db.json')
 const app = express();
 const PORT = 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+app.get('/api/notes', (req, res) => {
+    return res.json(notes);
+  });
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
