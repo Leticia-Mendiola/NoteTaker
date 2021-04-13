8c85dff8c5c8019513f85a8754ed792fde225ef0
Leticia-Mendiola <lmendiola509@gmail.com>
2021-04-12 22:19:22 -0500

    linked index.html to server.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const notes = require('./db/db.json')
+const home = require('./public/index.html')
 const app = express();
 const PORT = 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get('/', (req, res) => res.send(home));
 
 app.get('/api/notes', (req, res) => {
     return res.json(notes);
