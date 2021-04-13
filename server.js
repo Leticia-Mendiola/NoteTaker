685df8048baa9731fa633f2df703549ff6cd7778
Leticia-Mendiola <lmendiola509@gmail.com>
2021-04-12 22:42:41 -0500

    successfully added home page to server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const notes = require('./db/db.json');
+// const notes = require('./db/db.json');
 
 const app = express();
 const PORT = 8080;
@@ -9,7 +9,7 @@ const PORT = 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/assets/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
 app.get('/api/notes', (req, res) => res.json(notes));
 
